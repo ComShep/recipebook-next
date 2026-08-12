1a67dedc27cc991c43a8b732540e31141e37d3e6
Sergey Akishin <morales66@mail.ru>
2026-08-12 11:51:37 +0300

    Добавлен страница со списком рецептов

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,5 +1,13 @@
-export default function recipes() {
-  return (
-    <div>recipes</div>
-  )
+import { getFetchData } from "@/api/api";
+import { Recipes } from "@/components/widgets/recipes/Recipes";
+import { RecipesSectionResponse } from "@/types/types";
+import { transformResponse } from "@/utils/transformResponce";
+
+export default async function recipes() {
+  const recipesData = await getFetchData<RecipesSectionResponse>("recipes", {
+    revalidate: 3600,
+  });
+  const recipes = transformResponse(recipesData)
+
+  return <Recipes recipes={recipes} />;
 }
